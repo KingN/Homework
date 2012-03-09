d7a94cafed75ebcdde23cb86628d57f1ccddacf1
Daniele Spidalieri <lemo_dii@hotmail.com>
2012-03-09 16:44:51 +0100

    Update hw1.js

--- hw1.js
+++ hw1.js
@@ -1,27 +1,52 @@
-function Point(x,y){
-    this.x = x;
-    this.y = y;
-    this.getX = function (){
-    	return this.x;
-    } 
-    this.getY= function (){
-    	return this.y;
-    }
-    this.getDistance = function (punto){
-       var diffX=punto.getX() - this.getX();
-       var diffY=punto.getY() - this.getY();
-       var num = Math.pow(diffX,2) + Math.pow(diffY,2);
-       return Math.sqrt(num);
-    }
+/* POINT */
+
+var Point = function (x, y) {
+  this.x = x || 0;
+  this.y = y || 0;
 }
 
-function Triangle(p1,p2,p3){
-	this.getPerimeter = function (){
-		return (p1.getDistance(p2) + p2.getDistance(p3) + p3.getDistance(p1));
-	}
-	this.getArea = function (){
-		var semip = this.getPerimeter()/2;
+var sqrt = Math.sqrt;
+var pow = Math.pow;
 
-		return Math.sqrt(semip*(semip-p1.getDistance(p2))*(semip-p2.getDistance(p3))*(semip- p3.getDistance(p1)));
-	}
+Point.prototype.getDistance = function(point) {
+  return sqrt(pow(point.y - this.y, 2) + pow(point.x - this.y, 2));
+};
+
+
+/* TRIANGLE */
+
+var Triangle = function (p1, p2, p3) {
+  this.p1 = p1;
+  this.p2 = p2;
+  this.p3 = p3;
+
+  this.l1 = p2.getDistance(p3);
+  this.l2 = p3.getDistance(p1);
+  this.l3 = p1.getDistance(p2);
 }
+
+Triangle.prototype.getPerimeter = function() {
+  return this.l1 + this.l2 + this.l3;
+};
+
+Triangle.prototype.getArea = function() {
+  var p = this.getPerimeter() / 2;
+
+  return sqrt(p*(p - this.l1)*(p - this.l2)*(p - this.l3));
+};
+
+
+var x1 = 0, y1 = 0;
+var x2 = 10, y2 = 0;
+var x3 = 0, y3 = 10;
+
+var p1 = new Point(x1, y1);
+var p2 = new Point(x2, y2);
+var p3 = new Point(x3, y3);
+
+var d = p1.getDistance(p2);
+
+var t = new Triangle(p1,p2,p3);
+
+var perimetro = t.getPerimeter();
+var area = t.getArea();
