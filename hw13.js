ffcf90f40c1c9efe80bd0f2137c6401eab5d20f7
Daniele Spidalieri <lemo_dii@hotmail.com>
2012-03-13 14:31:19 +0100

    update hw13.js

--- hw13.js
+++ hw13.js
@@ -3,5 +3,14 @@
 /* è sopra,sotto,intersecato alla retta.*/
 
 Triangle.prototype.above = function(line) {
-	
+	return this.p1.membership(line) + this.p2.membership(line) + this.p3.membership(line) === 3;
+};
+
+Triangle.prototype.below = function(line){
+  return this.p1.membership(line) + this.p2.membership(line) + this.p3.membership(line) === - 3; 
+};
+
+Triangle.prototype.below = function(line){
+  var result = this.p1.membership(line) + this.p2.membership(line) + this.p3.membership(line)
+  return  result < 3 && result > -3; 
 };
