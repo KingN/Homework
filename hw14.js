886ac393fac910e38feeec36e40a8ce72d54b679
Daniele Spidalieri <lemo_dii@hotmail.com>
2012-03-13 14:46:24 +0100

    homeworks

---
+++ hw14.js
@@ -0,0 +1,6 @@
+/* * Refactoring della classe Triangle affinchè i tre vertici vengano memorizzati in un'array 
+chiamato points  */
+
+var Triangle = function (p1,p2,p3){
+	this.points = [p1,p2,p3];
+}
